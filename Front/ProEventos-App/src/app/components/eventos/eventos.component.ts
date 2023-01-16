6f87e4cea2a0cdb148b96e8768068b9279d198c2
Diego Teixeira <diegosmteixeira@gmail.com>
2023-01-16 11:40:14 -0300

    Reactive forms, FormGroup, FormControl and validators

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
-import { Evento } from '../../models/Evento';
-import { EventoService } from '../../services/evento.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
@@ -11,65 +8,4 @@ import { EventoService } from '../../services/evento.service';
 })
 
 export class EventosComponent {
-
-  // modalRef?: BsModalRef;
-  // public eventos: Evento[] = [];
-  // public eventosFiltrados: Evento[] = [];
-  // public widthImg: number = 150;
-  // public marginImg: number = 2;
-
-  // public isCollapsed: boolean = false;
-  // private _filtroLista: string = "";
-
-  // constructor(
-  //   private eventoService: EventoService,
-  //   private modalService: BsModalService
-  //   ) { }
-
-  // public ngOnInit() : void {
-  //   this.getEventos();
-  // }
-
-  // public getEventos(): void {
-  //   this.eventoService.getEventos().subscribe(
-  //     (_eventos : Evento[]) => {
-  //       this.eventos = _eventos,
-  //       this.eventosFiltrados = this.eventos;
-  //     },
-  //     error => console.log(error)
-  //   );
-  // }
-
-  // public get filtroLista() : string {
-  //   return this._filtroLista;
-  // }
-
-  // public set filtroLista(value : string) {
-  //   this._filtroLista = value;
-  //   this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  // }
-
-  // public filtrarEventos(filtrarPor : string) : Evento[] {
-  //   filtrarPor = filtrarPor.toLocaleLowerCase();
-  //   return this.eventos.filter(
-  //     (evento: { tema : string; local: string; }) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-  //     evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-  //   )
-  // }
-
-  // public change() : void {
-  //   this.isCollapsed = !this.isCollapsed;
-  // }
-
-  // openModal(template: TemplateRef<any>) {
-  //   this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  // }
-
-  // confirm(): void {
-  //   this.modalRef?.hide();
-  // }
-
-  // decline(): void {
-  //   this.modalRef?.hide();
-  // }
 }
