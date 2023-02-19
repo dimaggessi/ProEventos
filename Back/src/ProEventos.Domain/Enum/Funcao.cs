8a7594d101cfd29f81ab78265bb2b43e77766c16
Diego Teixeira <diegosmteixeira@gmail.com>
2023-02-19 19:32:48 -0300

    Role, User, UserRole - .NET Identity

---
+++ Back/src/ProEventos.Domain/Enum/Funcao.cs
@@ -0,0 +1,10 @@
+namespace ProEventos.Domain.Enum
+{
+    public enum Funcao
+    {
+        //Função do Usuário, (Palestrante|| Participante)
+        NaoInformado,
+        Participante,
+        Palestrante
+    }
+}
